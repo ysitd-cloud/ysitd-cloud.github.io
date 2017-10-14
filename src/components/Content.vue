fa70c6fa5f3e809862cccbf5b2dea8c8dadd8460
Tony Yip <tony@opensource.hk>
2017-10-15 01:20:09 +0800

    Add more detail

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,19 +1,48 @@
 <template>
-  <v-layout row>
-    <v-flex xs12>
-      <v-card>
-        <v-card-title primary-title>
-          <div>
-            <h3 class="headline">Introduction</h3>
-          </div>
-        </v-card-title>
-        <v-card-text>
-          <p>
-            This is an PaaS solution built by <a href="http://ysitd.io/" target="_blank">YSITD Community</a>.
-            It is using <a href="http://k8s.io/" target="_blank">Kubernetes</a> as the base platform.
-          </p>
-        </v-card-text>
-      </v-card>
-    </v-flex>
+  <v-layout column>
+    <v-layout row class="my-2">
+      <v-flex xs12>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline">Introduction</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            <p>
+              This is an PaaS solution built by <a href="http://ysitd.io/" target="_blank">YSITD Community</a>.
+              It is using <a href="http://k8s.io/" target="_blank">Kubernetes</a> as the base platform.
+            </p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row class="my-2">
+      <v-flex xs12>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline">Current Status</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>It is still not yet completely open for use.</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row class="my-2">
+      <v-flex xs12>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline">Development</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+            You may checkout <a href="https://develop.ysitd.cloud">Development Status</a>
+            or <a href="https://github.com/ysitd-cloud">YSITD Cloud Github</a>.
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-layout>
 </template>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -3,7 +3,8 @@
     <v-toolbar-title>YSITD Cloud</v-toolbar-title>
     <v-spacer />
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat herf="https://account.ysitd.cloud/">Login</v-btn>
+      <v-btn flat href="https://account.ysitd.cloud/">Login</v-btn>
+      <v-btn flat href="https://github.com/ysitd-cloud">Github</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
